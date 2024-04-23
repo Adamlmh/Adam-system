e00f67a8c65bd667e9089f16d261cbac9f086589
Adamlmh <13798755865@163.com>
2024-04-23 10:04:40 +0800

    暂存

---
+++ backend/app/models/Comment.js
@@ -0,0 +1,44 @@
+// models/Comment.js
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define(
+    "Comment",
+    {
+      commentId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      minutesId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      commenterId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      commenterName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      commentContent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      parentCommentId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      likeCount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+    },
+    {
+      timestamps: true,
+      timezone: "+08:00",
+    }
+  );
+
+  return Comment;
+};
